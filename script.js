29700cd192d8a6f0ce5a9f08455de788fed8db47
Francisco Javier G. Gastelum <bayoinths@gmail.com>
2024-07-22 17:27:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var users = {
+    'Ana Villanueva': {
+        password: '1234',
+        saldo: 500
+    },
+    'Javier Dub': {
+        password: 'estonofuetanfacil',
+        saldo: 100
+    },
+    'Ian': {
+        password: '4ever',
+        saldo: 750
+    },
+    'sol': {
+        password: 'nakagawa',
+        saldo: 300
+    }
+};
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    if (users.hasOwnProperty(username) && users[username].password === password) {
+        // Almacenamos el nombre de usuario y saldo en sessionStorage
+        sessionStorage.setItem('loggedInUser', username);
+        sessionStorage.setItem('saldo', users[username].saldo.toString());
+        
+        window.location.href = 'cajero.html';
+    } else {
+        document.getElementById('loginStatus').textContent = 'Usuario o contraseña incorrectos. Intenta de nuevo.';
+    }
+});
